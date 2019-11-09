e14ab298b5cc9d53dbe35c57f7a80ac782c6b735
Augusto <aavila@verointernet.com.br>
2019-11-09 15:41:46 -0300

    terminado parte de cadastro de usuário

--- login.php
+++ login.php
@@ -24,7 +24,7 @@
     } else {
         $_SESSION['nao_autenticado'] = true;
         
-        header('Location: index.php');
+        header('Location: index.html');
         exit();
     }
         
